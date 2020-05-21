2f57362b7ff938af123c1b3c82604845ae37031d
lu.jin(卢进) <1395856829@qq.com>
2020-05-21 15:10:26 +0800

    fix bug

--- pkg/utils/tlsCrt.go
+++ pkg/utils/tlsCrt.go
@@ -6,20 +6,20 @@ import (
 	"strings"
 )
 
-func GetTlsCrt(secret string, namespace string)(err error) {
-	command :=`kubectl get secret ` + secret + ` -n ` + namespace + ` -o json|jq '.data."tls.crt"'`
+func GetTlsCrt(secret string, namespace string) (err error) {
+	command := `kubectl get secret ` + secret + ` -n ` + namespace + ` -o json|jq '.data."tls.crt"'`
 	output, err := Cmd(command)
 	if err != nil {
 		return err
 	}
-	tlsCrtBase64 := strings.Replace(string(output),"\"","",-1)
+	tlsCrtBase64 := strings.Replace(string(output), "\"", "", -1)
 	decodeBytes, err := base64.StdEncoding.DecodeString(tlsCrtBase64)
 	if err != nil {
 		return err
 	}
 	var data = []byte(decodeBytes)
 	err2 := ioutil.WriteFile(secret, data, 0666)
-	if err != nil {
+	if err2 != nil {
 		return err2
 	}
 	return nil
